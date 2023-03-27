19ca3da9684f17783eb6d0614be6579b1fc2ce83
Igor Ostrohliad <sana22_oid@student.ztu.edu.ua>
2023-03-27 10:18:44 +0300

    Create new controller, model and view for hello_form

---
+++ SanaTask1/Controllers/Task1Controller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SanaTask1.Models;
+
+namespace SanaTask1.Controllers
+{
+    public class Task1Controller : Controller
+    {
+        public IActionResult Index(User user)
+        {
+            return View(user);
+        }
+    }
+}
---
+++ SanaTask1/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace SanaTask1.Models
+{
+    public class User
+    {
+        protected string name;
+        public string Name {
+            get { return name; }
+            set { name = "Hello, " + value; }
+        }
+    }
+}
